9dcae99cdc2e72fb1b0d42d7a68531441c5257ca
Roman Dolgopolsky <rdolgopolsky@gmail.com>
2023-08-22 16:33:06 +0300

    changes to start working

--- src/js/jquery.main.js
+++ src/js/jquery.main.js
@@ -1,8 +1,8 @@
-let $ = jQuery.noConflict();
-$(function() {
-	isElementExist(".nav-drop", initSmartMenu);
-	jcfInit();
-});
+let $ = jQuery.noConflict()
+$(function () {
+	isElementExist('.nav-drop', initSmartMenu)
+	jcfInit()
+})
 
 //-------- -------- -------- --------
 //-------- js custom start
@@ -10,68 +10,73 @@ $(function() {
 
 // Helper if element exist then call function
 function isElementExist(_el, _cb) {
-	const elem = document.querySelector(_el);
+	const elem = document.querySelector(_el)
 
 	if (document.body.contains(elem)) {
 		try {
-			_cb();
-		} catch(e) {
-			console.log(e);
+			_cb()
+		} catch (e) {
+			console.log(e)
 		}
 	}
 }
 
 // initialize custom form elements (checkbox, radio, select) https://github.com/w3co/jcf
 function jcfInit() {
-	const customSelect = $('select');
-	const customCheckbox = $('input[type="checkbox"]');
-	const customRadio = $('input[type="radio"]');
+	const customSelect = $('select')
+	const customCheckbox = $('input[type="checkbox"]')
+	const customRadio = $('input[type="radio"]')
 
 	// all option see https://github.com/w3co/jcf
 	jcf.setOptions('Select', {
 		wrapNative: false,
 		wrapNativeOnMobile: false,
 		fakeDropInBody: false,
-		maxVisibleItems: 6
-	});
+		maxVisibleItems: 6,
+	})
 
-	jcf.setOptions('Checkbox', {});
+	jcf.setOptions('Checkbox', {})
 
-	jcf.setOptions('Radio', {});
+	jcf.setOptions('Radio', {})
 
 	// init only after option
-	jcf.replace(customSelect);
-	jcf.replace(customCheckbox);
-	jcf.replace(customRadio);
-
+	jcf.replace(customSelect)
+	jcf.replace(customCheckbox)
+	jcf.replace(customRadio)
 }
 
 // smart menu init
 function initSmartMenu() {
-	const headerMenu  = $(".page-header-menu");
-	const headerMenuWrapper = $(".page-header-menu-wrapper");
-	const nav = $(".nav");
-	const body = $("body");
+	const headerMenu = $('.page-header-menu')
+	const headerMenuWrapper = $('.page-header-menu-wrapper')
+	const nav = $('.nav')
+	const body = $('body')
 
-	const distanceBetweenMenuAndNav = headerMenuWrapper.offset().top + headerMenuWrapper.innerHeight() - nav.offset().top - nav.innerHeight();
+	const distanceBetweenMenuAndNav =
+		headerMenuWrapper.offset().top +
+		headerMenuWrapper.innerHeight() -
+		nav.offset().top -
+		nav.innerHeight()
 
 	headerMenu.smartmenus({
-		collapsibleBehavior: "accordion",
-		mainMenuSubOffsetY: distanceBetweenMenuAndNav
-	});
+		collapsibleBehavior: 'accordion',
+		mainMenuSubOffsetY: distanceBetweenMenuAndNav,
+	})
 
 	// changed date attribute to a class (need to reverse last item menu)
-	headerMenu.children().last().addClass('nav-sm-reverse');
+	headerMenu.children().last().addClass('nav-sm-reverse')
 
 	body.mobileNav({
-		menuActiveClass: "nav-active",
-		menuOpener: ".nav-opener",
+		menuActiveClass: 'nav-active',
+		menuOpener: '.nav-opener',
 		hideOnClickOutside: true,
-		menuDrop: ".nav-drop"
-	}), "ontouchstart" in document.documentElement ||
-	$(window).on("resize orientationchange", function() {
-		body.removeClass("nav-active"), $.SmartMenus.hideAll();
-	});
+		menuDrop: '.nav-drop',
+	})
+	'ontouchstart' in document.documentElement ||
+		$(window).on('resize orientationchange', function () {
+			body.removeClass('nav-active')
+			$.SmartMenus.hideAll()
+		})
 }
 
 //-------- -------- -------- --------
